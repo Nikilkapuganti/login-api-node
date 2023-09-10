13aefc100105e9519b047c6a987852f098092dda
nkapuganti <nkapuganti@crystallinesoft.com>
2023-09-10 08:48:02 +0530

    first commit

---
+++ auditor/auditor.controller.js
@@ -0,0 +1,14 @@
+﻿const express = require('express');
+const router = express.Router();
+const auditService = require('./auditor.service');
+
+// routes
+router.get('/', getAll);
+
+module.exports = router;
+
+function getAll(req, res, next) {
+    auditService.getAll()
+    .then(user => user ? res.json(user) : res.status(401).json({ message: 'No access' }))
+    .catch(err => next(err));
+}
---
+++ auditor/auditor.service.js
@@ -0,0 +1,20 @@
+﻿const config = require('config.js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+const User = db.User;
+
+module.exports = {
+    getAll
+   
+};
+
+async function getAll() {
+    const condition = {
+                $and: [
+                  { lastLoginDate: { $exists: true } },
+                  { clientIp: { $exists: true } }
+                ]
+              };
+    return await User.find(condition).select('-hash');
+}
